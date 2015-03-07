c24e88e2229aa147e0a69ea74f9178ed65aa72fc
idaelDev <judicael.abecassis@gmail.com>
2015-03-07 21:18:44 +0100

    Scene de tournage

---
+++ Assets/Scripts/CameraSwitchScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitchScript : MonoBehaviour {
+
+    Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+            cam.enabled = !cam.enabled;
+	}
+}
